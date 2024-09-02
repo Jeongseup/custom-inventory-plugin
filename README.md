77ffc3c5efc7204fa417b9e4e8cba029e597b948
Jeongseup <seup87@gmail.com>
2024-09-02 18:01:44 +0900

    Add memo

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 ## Demo repository for custom inventory plugins
+
 Please see: https://termlen0.github.io/2019/11/16/observations/ for details
+
+### Commands
+
+```bash
+# check inventory list
+ansible-inventory -i csv_inventory.yaml --playbook-dir ./ --list
+
+# check inventory graph
+ansible-inventory -i csv_inventory.yaml --playbook-dir ./ --graph
+
+# run playbook
+ansible-playbook -i csv_inventory.yaml playbook.yaml
+
+ANSIBLE_INVENTORY_ENABLED=my_csv_plugin ansible-inventory -i csv_inventory.yml --list
+```
+
+### VScode Extensions...
+
+- https://velog.io/@kshjessica/VSCode-%EC%97%90%EC%84%9C-Prettier-%EB%9E%91-Black-%EB%8F%99%EC%8B%9C%EC%97%90-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
--- inventory_plugins/my_csv_plugin.py
+++ inventory_plugins/my_csv_plugin.py
@@ -1,6 +1,8 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+
+# The DOCUMENTATION section is required and used by the plugin. 
 DOCUMENTATION = r'''
     name: my_csv_plugin
     plugin_type: inventory
@@ -29,21 +31,38 @@ import csv
 class InventoryModule(BaseInventoryPlugin):
     NAME = 'my_csv_plugin'
 
+    '''
+    Ansible expects the custom plugin to implement at least the parse method and recommends the verify_file method 
+    as a way to validate user input. 
+    '''
 
     def verify_file(self, path):
-        '''Return true/false if this is possibly a valid file for this plugin to
-consume
-
         '''
-        valid = False
+        Return true/false if this is a valid file for this plugin to consume
+        '''
         if super(InventoryModule, self).verify_file(path):
-            # base class verifies that file exists and is readable by current
-            # user
-            if path.endswith(('csv_inventory.yaml',
-                              'csv_inventory.yml')):
+            #base class verifies that file exists 
+            #and is readable by current user
+            if path.endswith(('csv_inventory.yaml','csv_inventory.yml')):
                 valid = True
         return valid
-
+    
+    def parse(self, inventory, loader, path, cache):
+          '''Return dynamic inventory from source '''
+          super(InventoryModule, self).parse(inventory, loader, path, cache)
+          # Read the inventory YAML file
+          self._read_config_data(path)
+          try:
+              # Store the options from the YAML file
+              self.plugin = self.get_option('plugin')
+              self.inv_dir = self.get_option('path_to_inventory')
+              self.inv_file = self.get_option('csv_file')
+          except Exception as e:
+              raise AnsibleParserError(
+                  'All correct options required: {}'.format(e))
+          # Call our internal helper to populate the dynamic inventory
+          self._populate()
+          
     def _get_structured_inventory(self, inventory_file):
     
         #Initialize a dict
@@ -56,6 +75,19 @@ consume
                 inventory_data[hostname] = rows
         return inventory_data
 
+    # step. 1
+    # def _populate(self):
+    #     '''Return the hosts and groups'''
+    #     import pdb; pdb.set_trace()
+
+    # step. 2
+    # def _populate(self):
+    #     '''Return the hosts and groups'''
+    #     self.inventory_file = self.inv_dir + '/' + self.inv_file
+    #     self.myinventory = self._get_structured_inventory(self.inventory_file)
+    #     # import pdb; pdb.set_trace()
+
+    # step. 3
     def _populate(self):
         '''Return the hosts and groups'''
         self.inventory_file = self.inv_dir + '/' + self.inv_file
@@ -84,20 +116,4 @@ consume
             self.inventory.add_host(host=hostname, group=data['Platform'])
             self.inventory.set_variable(hostname, 'ansible_host', data['Mgmt IP'])
             self.inventory.set_variable(hostname, 'ansible_network_os', data['Platform'])
-            
-    
-    def parse(self, inventory, loader, path, cache):
-       '''Return dynamic inventory from source '''
-       super(InventoryModule, self).parse(inventory, loader, path, cache)
-       # Read the inventory YAML file
-       self._read_config_data(path)
-       try:
-           # Store the options from the YAML file
-           self.plugin = self.get_option('plugin')
-           self.inv_dir = self.get_option('path_to_inventory')
-           self.inv_file = self.get_option('csv_file')
-       except Exception as e:
-           raise AnsibleParserError(
-               'All correct options required: {}'.format(e))
-       # Call our internal helper to populate the dynamic inventory
-       self._populate()
+                
